71c523c940faa824f3a273fa13bb8c710846714a
Richard <programmingislife23@gmail.com>
2024-07-04 19:32:24 -0300

    colocando style

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,10 @@
 
 <script setup>
-  import Navbar from "@/components/Navbar.vue"
+  import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <Navbar />
 </template>
 
 
